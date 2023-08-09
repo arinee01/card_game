4130e4e9ad6f09e6a05b06906803e6ebaf6e97af
Arina Gracheva <arinee.msc@gmail.com>
2023-08-09 23:50:38 +0300

    не работает кнопка

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-const choiceInputs = document.querySelectorAll(".difficulty__box_input");
-const choiceButton = document.querySelector(".difficulty__box_button");
+import { cardsForRandom } from "./randomCards.js";
+import { randerFirstPage, imia } from "./render.js";
+import "./style.css";
 
-import { randerGameBoard } from "./render.js";
+randerFirstPage();
+
+const gameCards = [];
 
 export let difficulty = "";
+
+let choiceInputs = document.querySelectorAll(".difficulty__box_input");
+let choiceButton = document.querySelector(".difficulty__box_button");
 
 for (let choiceInput of choiceInputs) {
     choiceInput.addEventListener("click", function (e) {
@@ -17,4 +23,52 @@ function buttonDisabled(difficulty) {
     }
 }
 
-choiceButton.addEventListener("click", randerGameBoard);
+choiceButton.addEventListener("click", function () {
+    console.log(difficulty);
+    let cardsForGame = cardsForRandom.slice();
+    imia(cardsForGame);
+});
+
+export function hideWhatTheCard() {
+    const cards = document.querySelectorAll(".card");
+    cards.forEach((card) => {
+        card.classList.add("game__board_card");
+    });
+}
+
+export function ListnerClicksInGame() {
+    const cards = document.querySelectorAll(".card");
+
+    for (let i = 0; i < cards.length; i++) {
+        cards[i].addEventListener("click", function (event) {
+            cards[i].classList.remove("game__board_card");
+            gameCards.push(event.target.id);
+            console.log(event.target.id);
+            setTimeout(checkGameResult, 40);
+        });
+    }
+}
+
+function checkGameResult() {
+    if (!gameCards[1]) {
+        return;
+    }
+    if (gameCards[1] === gameCards[0]) {
+        alert("Ты победил");
+        setTimeout(gameCards.splice(0, 2), 400);
+    }
+    if (gameCards[1] !== gameCards[0]) {
+        alert("Ты проиграл");
+        setTimeout(gameCards.splice(0, 2), 400);
+    } else {
+        return;
+    }
+}
+
+export function checkClick(button) {
+    button.addEventListener("click", function () {
+        console.log(difficulty);
+        let cardsForGame = cardsForRandom.slice();
+        imia(cardsForGame);
+    });
+}
--- render.js
+++ render.js
@@ -1,6 +1,68 @@
-import { difficulty } from "./index.js";
+import {
+    difficulty,
+    hideWhatTheCard,
+    ListnerClicksInGame,
+    checkClick,
+} from "./index.js";
 
-export function randerGameBoard() {
+export function randerFirstPage() {
+    const body = document.querySelector("body");
+    body.innerHTML = ` <main class="difficulty">
+    <section class="difficulty__box">
+        <p class="difficulty__box_text">
+            Выбери <br />
+            сложность
+        </p>
+        <article class="difficulty__box_difficulst">
+            <div>
+                <input
+                    type="radio"
+                    value="1"
+                    id="light__difficult"
+                    name="choice"
+                    class="difficulty__box_input"
+                />
+                <label
+                    for="light__difficult"
+                    class="difficulty__box_choice difficulty__box_light"
+                    >1</label
+                >
+            </div>
+            <div>
+                <input
+                    type="radio"
+                    value="2"
+                    id="mid__difficult"
+                    name="choice"
+                    class="difficulty__box_input"
+                />
+                <label
+                    for="mid__difficult"
+                    class="difficulty__box_choice difficulty__box_mid"
+                    >2</label
+                >
+            </div>
+            <div>
+                <input
+                    type="radio"
+                    value="3"
+                    id="hard__difficult"
+                    name="choice"
+                    class="difficulty__box_input"
+                />
+                <label
+                    for="hard__difficult"
+                    class="difficulty__box_choice difficulty__box_hard"
+                    >3</label
+                >
+            </div>
+        </article>
+        <button class="difficulty__box_button" disabled>Старт</button>
+    </section>
+</main>`;
+}
+
+export function randerGameBoard(cardsForGame) {
     console.log(`сложность ${difficulty}`);
     const body = document.querySelector("body");
     body.innerHTML = `<main class="game">
@@ -15,88 +77,53 @@ export function randerGameBoard() {
         <button class="game__header_button">Начать заново</button>
     </header>
     <section class="game__board">
-        <div class="game__board_card"></div>
-        <div class="game__board_card"></div>
-        <div class="game__board_card"></div>
-        <div class="game__board_card"></div>
-        <div class="game__board_card"></div>
-        <div class="game__board_card"></div>
-        <div class="game__board_card"></div>
-        <div class="game__board_card"></div>
-        <div class="game__board_card"></div>
-        <div class="game__board_card"></div>
-        <div class="game__board_card"></div>
-        <div class="game__board_card"></div>
-        <div class="game__board_card"></div>
-        <div class="game__board_card"></div>
-        <div class="game__board_card"></div>
-        <div class="game__board_card"></div>
-        <div class="game__board_card"></div>
-        <div class="game__board_card"></div>
-        <div class="game__board_card"></div>
-        <div class="game__board_card"></div>
-        <div class="game__board_card"></div>
-        <div class="game__board_card"></div>
-        <div class="game__board_card"></div>
-        <div class="game__board_card"></div>
-        <div class="game__board_card"></div>
-        <div class="game__board_card"></div>
-        <div class="game__board_card"></div>
-        <div class="game__board_card"></div>
-        <div class="game__board_card"></div>
-        <div class="game__board_card"></div>
-        <div class="game__board_card"></div>
-        <div class="game__board_card"></div>
-        <div class="game__board_card"></div>
-        <div class="game__board_card"></div>
-        <div class="game__board_card"></div>
-        <div class="game__board_card"></div>
+        ${cardsForGame}
     </section>
 </main>`;
     const button = document.querySelector(".game__header_button");
     checkClick(button);
+    setTimeout(hideWhatTheCard, 5000);
+    setTimeout(ListnerClicksInGame, 5000);
 }
 
-function checkClick(button) {
-    button.addEventListener("click", randerCardsBoard);
+let cardsForGame = [];
+
+function perebor(cards, lengthArr) {
+    console.log(cards.length);
+    const elem = Math.floor(Math.random() * lengthArr);
+    console.log(cards);
+    cardsForGame.push(cards[elem]);
+    console.log(cards[elem]);
+    cards.splice(elem, 1);
+    return cardsForGame;
 }
 
-function randerCardsBoard() {
-    const board = document.querySelector(".game__board");
-    board.innerHTML = `        <div class="card-1"></div>
-    <div class="card-2"></div>
-    <div class="card-3"></div>
-    <div class="card-4"></div>
-    <div class="card-5"></div>
-    <div class="card-6"></div>
-    <div class="card-7"></div>
-    <div class="card-8"></div>
-    <div class="card-9"></div>
-    <div class="card-10"></div>
-    <div class="card-11"></div>
-    <div class="card-12"></div>
-    <div class="card-13"></div>
-    <div class="card-14"></div>
-    <div class="card-15"></div>
-    <div class="card-16"></div>
-    <div class="card-17"></div>
-    <div class="card-18"></div>
-    <div class="card-19"></div>
-    <div class="card-20"></div>
-    <div class="card-21"></div>
-    <div class="card-22"></div>
-    <div class="card-23"></div>
-    <div class="card-24"></div>
-    <div class="card-25"></div>
-    <div class="card-26"></div>
-    <div class="card-27"></div>
-    <div class="card-28"></div>
-    <div class="card-29"></div>
-    <div class="card-30"></div>
-    <div class="card-31"></div>
-    <div class="card-32"></div>
-    <div class="card-33"></div>
-    <div class="card-34"></div>
-    <div class="card-35"></div>
-    <div class="card-36"></div>`;
+export function imia(cards) {
+    let lengthArr = "";
+    if (difficulty === "1") {
+        lengthArr = 6;
+    }
+    if (difficulty === "2") {
+        lengthArr = 12;
+    }
+    if (difficulty === "3") {
+        lengthArr = 18;
+    }
+
+    for (let index = 0; index < lengthArr; index++) {
+        perebor(cards, lengthArr);
+        let allCardsForGame = "";
+        if (index === lengthArr - 1) {
+            console.log(cardsForGame);
+            allCardsForGame = [...cardsForGame, ...cardsForGame];
+            console.log(allCardsForGame);
+            randomize(allCardsForGame);
+        }
+    }
+}
+
+function randomize(array) {
+    array.sort(() => Math.random() - 0.5);
+    randerGameBoard(array.join(" "));
+    cardsForGame = [];
 }
